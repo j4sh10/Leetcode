7639864dedab5f79670a899aa43f2b94f17ba13b
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2024-12-24 19:55:00 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (7.85%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -5,6 +5,4 @@ class Solution:
             employee_hour = hours[i]
             if employee_hour >= target:
                 count_of_good_employees += 1
-            else:
-                count_of_good_employees += 0
         return count_of_good_employees
